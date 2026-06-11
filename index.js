0fe4d23619a4443e5473ed0d6ded18d4e79a410e
Samincode01 <samin990bd@gmail.com>
2026-06-11 21:22:08 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://drivefleet:tnAG7CASiwZP7g6j@cluster0.coyimpf.mongodb.net/?appName=Cluster0";
+dotenv.config()
+const uri = process.env.MONGODB_URI;
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT;
 
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req,res)=>{
     res.send('Server is running fine')
